e4a82a6b0d5b3d2441caffb93c8aba9bedba6f0e
Roshdy <roshdy89@gmail.com>
2018-01-31 12:35:36 +0300

    Revert "Fix"
    This reverts commit 21d3b6e6a9d708a0cb67b07bde9e95973a9dc73a.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'roshdy:reactive-countdown',
-  version: '0.0.4',
+  version: '0.0.3',
   summary: 'A simple reactive countdown timer for Meteor',
   git: 'https://github.com/roshdy-sk/meteor-reactive-countdown.git',
   documentation: 'README.md'
